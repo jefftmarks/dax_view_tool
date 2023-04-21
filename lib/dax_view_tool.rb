980ad14b99254644103f51b36a048270394c9936
Jeff Marks <jeff.t.marks@gmail.com>
2023-04-21 15:09:29 -0400

    Dev tool initial commit

---
+++ lib/dax_view_tool.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require_relative "dax_view_tool/version"
+require_relative "dax_view_tool/renderer"
+
+module DaxViewTool
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/dax_view_tool_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe DaxViewTool do
+  it "has a version number" do
+    expect(DaxViewTool::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
